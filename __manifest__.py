ae6328c61148b793107d463902e344f15c18e76b
EwetoyeIbrahim <i.ewetoye@gmail.com>
2021-04-15 08:57:32 +0100

    Basic module with templates

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Academy',
+    'version': '0.1',
+    'summary': 'Build a website in Odoo',
+    'category': 'Website/Website',
+    'license': 'AGPL-3',
+    'author': 'Ewetoye Ibrahim',
+    'website': 'https://EwetoyeIbrahim.github.io',
+    'description': '''
+    This module is used to show how to build a website
+    in Odoo ERP system
+    ''',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates.xml',
+    ],
+    'installable': True,
+}
---
+++ controllers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        return http.request.render('academy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
