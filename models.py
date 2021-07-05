ab8035dffa51095f0587b3dc3581cfb97693f303
EwetoyeIbrahim <i.ewetoye@gmail.com>
2021-07-05 13:16:13 +0100

    Relationship between models

--- models.py
+++ models.py
@@ -5,3 +5,8 @@ class Teachers(models.Model):
     _name = 'academy.teachers'
     name = fields.Char()
     biography = fields.Html()
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
